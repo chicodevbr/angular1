72ec09a32dae84af31a68e85c631438b4d4c717b
fjunior <fjunior@gmail.com>
2025-02-22 19:06:49 +0000

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+}
