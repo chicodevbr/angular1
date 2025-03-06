6b7b3beae7124625b96bd64af59689576772bb8d
fjunior <fjunior@gmail.com>
2025-03-06 22:15:16 +0000

    adding module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
